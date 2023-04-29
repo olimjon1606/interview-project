8b488e8cda2a866e3b1e28cc2ebf48480740467c
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-29 21:59:02 +0200

    done now

--- circle app/app.js
+++ circle app/app.js
@@ -1,22 +1,40 @@
-const bodyEl = document.querySelector("body");
+const bodyEl = document.querySelector(".span-container");
 const colorString = ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-bodyEl.addEventListener("click", (event) => {
-    const x = event.offsetX;
-    const y = event.offsetY;
-    const spanEl = document.createElement("span");
+function createCircle(x, y) {
+    let spanEl = document.createElement("span");
     spanEl.style.left = x + "px";
     spanEl.style.top = y + "px";
     const size = randomNumber(10, 150);
+    spanEl.style.backgroundColor = randomColorCode()
     spanEl.style.width = size + "px";
     spanEl.style.height = size + "px";
-    spanEl.style.backgroundColor = randomColorCode()
-    console.log(randomColorCode())
+    spanEl.style.border = "solid 3px #fff";
+    spanEl.style.pointerEvents = "none"
     bodyEl.appendChild(spanEl);
-    setTimeout(() => {
-        spanEl.remove();
-    }, randomNumber(3000, 8000));
-});
+    return spanEl;
+}
+bodyEl.addEventListener("click", (event) => {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    spanEl = createCircle(x, y)
+    spanEl.style.position = "fixed";
+    let time = randomNumber(5000, 10000)
+    if (bodyEl.lastChild.previousElementSibling) {
+        setTimeout(() => {
+            bodyEl.lastChild.previousElementSibling.remove();
+        }, time);
+    }
+    console.log(time)
+
+    bodyEl.addEventListener("mousemove", (event) => {
+        const x = event.offsetX;
+        const y = event.offsetY;
+        spanEl.style.left = x + "px";
+        spanEl.style.top = y + "px";
+    })
+})
+
 function randomNumber(start, end) {
     return Math.ceil(Math.random() * (end - start)) + start
 }
